0fa3923e077be59858a66faa009934be6c44065b
Emanuel Fabio <efabio05@icloud.com>
2023-03-24 17:37:43 -0700

    Added text Data

--- falling-code.js
+++ falling-code.js
@@ -1,9 +1,40 @@
-var box = document.getElementsByClassName("square");
+"use strict";
 
-var animation = anime({
-    targets: box,
-    translateY: '250px',
-    duration: 9000,
-    loop: true,
-    autoplay: true   
-})
+const fallingCodeWrapper = document.getElementById("falling-code-wrapper");
+
+class fallingCode
+{
+    constructor(element)
+    {
+        this.element = document.createElement(element);
+        this.setTextOnElement("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+        var animation = anime({
+            targets: this.element,
+            translateY: '100vh',
+            duration: (Math.random() * 10000) + 4000,
+            delay: Math.random() * 1000,
+            easing: 'linear',
+            loop: true,
+            autoplay: true   
+        })
+    }
+
+    setTextOnElement(txt)
+    {
+        this.element.textContent = txt;
+    }
+
+    get Element()
+    {
+        return this.element;
+    }
+}
+
+for(let i = 0; i < 60; i++)
+{
+    fallingCodeWrapper.appendChild(new fallingCode("div").element);
+    
+}
+
+console.log(window.screen.height);
